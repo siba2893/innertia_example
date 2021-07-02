e38ca9499150ec4a02eb0b9384b165b19fc7b6c3
Daniel Sibaja <siba2893@gmail.com>
2021-07-02 13:21:01 -0600

    Initial Setup - Rails + Vue + Inertia

---
+++ app/javascript/packs/application.js
@@ -0,0 +1,33 @@
+// This file is automatically compiled by Webpack, along with any other files
+// present in this directory. You're encouraged to place your actual application logic in
+// a relevant structure within app/javascript and only use these pack files to reference
+// that code so it'll be compiled.
+
+import Rails from "@rails/ujs"
+import * as ActiveStorage from "@rails/activestorage"
+import "channels"
+
+Rails.start()
+ActiveStorage.start()
+
+// VUE INIT
+
+import Vue from 'vue'
+import { App, plugin } from '@inertiajs/inertia-vue'
+import { InertiaProgress } from '@inertiajs/progress'
+
+Vue.use(plugin)
+InertiaProgress.init()
+
+// Instead of using App.vue page, Inertia
+// will use Rails application.html.erb layout page
+const el = document.getElementById('app')
+
+new Vue({
+  render: h => h(App, {
+    props: {
+      initialPage: JSON.parse(el.dataset.page),
+      resolveComponent: name => require(`../pages/${name}`).default,
+    },
+  }),
+}).$mount(el)

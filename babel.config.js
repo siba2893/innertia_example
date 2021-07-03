e403cd882a18444200aa630e2de3de847d260f48
Daniel Sibaja <siba2893@gmail.com>
2021-07-02 20:07:47 -0600

    Add Extra Config to TailwindCSS

--- babel.config.js
+++ babel.config.js
@@ -64,6 +64,10 @@ module.exports = function(api) {
         {
           async: false
         }
+      ],
+      [
+        "@babel/plugin-proposal-private-methods",
+        { "loose": true }
       ]
     ].filter(Boolean)
   }
